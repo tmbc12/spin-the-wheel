688590d17790548aa1b6055dd3e6111247b5e7cb
yasar-developer <yasarconnects@gmail.com>
2025-01-06 21:06:48 +0530

    changed cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://spin-the-wheel-alpha.vercel.app/',
+    origin: '*',
     credentials: true
 }));
 
